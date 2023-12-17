940769e319f733a6282f2258880d450bc1cb1943
thanhbaole97 <thanhbaole97@gmail.com>
2023-12-17 23:59:43 +0700

    update vocabulary
    update vocabulary

--- js/word.js
+++ js/word.js
@@ -1,9 +1,9 @@
 const ENGLISH = {
-    'dog':'chó',
-    'cat':'mèo',
-    'shark':'cas maajp',
-    'banana':'chuoois',
-    'apple':'taos',
-    'tree':'caay',
-    'vegetable':'rau'
+    'Catastrophic':'bi thương/ thảm khốc',
+    'Bio-fertilizer':'phân bón sinh học',
+    'Alleviation':'làm nhẹ',
+    'Combine':'kết hợp',
+    'Condemn':'kết tội',
+    'Creativity':'tính sáng tạo',
+    'Ecological':'thuộc sinh thái'
 };
